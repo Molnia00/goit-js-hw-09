e6c0e8fe8ac5dd75f995d6d33942e8e971bdc904
Molnia00 <bilankati@gmail.com>
2024-10-28 13:24:17 +0100

    update

---
+++ src/js/2-form.js
@@ -0,0 +1,50 @@
+const storageKey = "feedback-form-state";
+const email = document.querySelector('.email-input');
+const message = document.querySelector('.textarea-A');
+const form = document.querySelector('.feedback-form');
+
+const formData = {
+    email: "",
+    message: ""
+}
+
+form.addEventListener('input', justInput);
+function justInput(event) {
+   const { name, value } = event.target;
+    formData[name] = value;
+    localStorage.setItem(storageKey, JSON.stringify(formData));
+}
+
+form.addEventListener('submit', subFunClear)
+function subFunClear(event) {
+    event.preventDefault();
+    event.currentTarget.reset();
+    localStorage.clear(storageKey)
+
+    if (!formData.email || !formData.message) {
+        alert('Fill please all fields');
+        return; 
+    }
+}
+
+getIteamfromLocalStorage()
+loadFormData()
+
+function getIteamfromLocalStorage() {
+    const sendedMess = localStorage.getItem(storageKey);
+    if (sendedMess) {
+        formData.value = sendedMess;
+    }
+}
+
+function loadFormData() {
+    const storedData = localStorage.getItem(storageKey);
+    if (storedData) {
+        const data = JSON.parse(storedData);
+        formData.email = data.email;
+        formData.message = data.message;
+
+        email.value = data.email;
+        message.value = data.message;
+    }
+}
